0e0d2e939edba21d17247f32d79ed195646ee2a6
Hassana <baazizihassana@gmail.com>
2022-01-04 16:59:03 +0100

    added test2

--- src/test/java/tp1.java
+++ src/test/java/tp1.java
@@ -19,12 +19,31 @@ public class tp1 {
         WebElement barreRecherche = driver.findElement(By.id("twotabsearchtextbox"));
         barreRecherche.sendKeys("machine a raclette");
         barreRecherche.sendKeys(Keys.ENTER);
-
-        //driver.quit();
+        driver.quit();
     }
 
     @Test
     public void test2(){
+        WebDriver driver = new ChromeDriver();
+        driver.get("https://www.amazon.fr");
+        driver.manage().window().maximize();
+        //Fermer cookies
+        WebElement buttonCookies = driver.findElement(By.id("sp-cc-accept"));
+        buttonCookies.click();
+        WebElement barreRecherche = driver.findElement(By.id("twotabsearchtextbox"));
+        barreRecherche.sendKeys("machine a raclette");
+        barreRecherche.sendKeys(Keys.ENTER);
+        try {
+            Thread.sleep(2000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
 
+        WebElement firstProduct = driver.findElement(By.cssSelector("[cel_widget_id='MAIN-SEARCH_RESULTS-11']"));
+        firstProduct.click();
+        
+        WebElement ajoutPanier= driver.findElement(By.cssSelector("[data-action='dp-pre-atc-declarative']"));
+        ajoutPanier.click();
+        driver.quit();
     }
 }
