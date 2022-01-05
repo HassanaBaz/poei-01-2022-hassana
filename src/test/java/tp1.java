5bc5398351c4c78e5340ea7d6458b58c8291633e
Hassana <baazizihassana@gmail.com>
2022-01-05 12:25:10 +0100

    implicit wait

--- src/test/java/tp1.java
+++ src/test/java/tp1.java
@@ -7,12 +7,16 @@ import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
+import java.time.Duration;
+
 public class tp1 {
 
     WebDriver driver;
     @BeforeMethod
     public void setup(){
         driver = new ChromeDriver();
+        //Attente implicite
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(2));
         driver.get("https://www.amazon.fr");
         driver.manage().window().maximize();
         //Fermer cookies
@@ -38,21 +42,15 @@ public class tp1 {
         WebElement barreRecherche = driver.findElement(By.id("twotabsearchtextbox"));
         barreRecherche.sendKeys("machine a raclette");
         barreRecherche.sendKeys(Keys.ENTER);
-        try {
-            Thread.sleep(2000);
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        }
+        //Attente implicite
+
         //Selectioner le premier article
         WebElement premierArticle = driver.findElement(By.cssSelector("[cel_widget_id='MAIN-SEARCH_RESULTS-10']"));
         premierArticle.click();
+
         //Ajouter l'article dans le panier
-        WebElement ajoutPanier= driver.findElement(By.cssSelector("[data-action='dp-pre-atc-declarative']"));
+        WebElement ajoutPanier= driver.findElement(By.cssSelector("[name='submit.add-to-cart']"));
         ajoutPanier.click();
-        try {
-            Thread.sleep(3000);
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        }
+
     }
 }
