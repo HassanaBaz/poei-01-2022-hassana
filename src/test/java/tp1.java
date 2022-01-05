ab6a6a5675a8393a9f45201ec241fef68fb6143f
Hassana <baazizihassana@gmail.com>
2022-01-05 15:12:06 +0100

    add test livres

--- src/test/java/tp1.java
+++ src/test/java/tp1.java
@@ -3,6 +3,8 @@ import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
@@ -51,6 +53,16 @@ public class tp1 {
         //Ajouter l'article dans le panier
         WebElement ajoutPanier= driver.findElement(By.cssSelector("[name='submit.add-to-cart']"));
         ajoutPanier.click();
+    }
+    @Test
+    public void testLivres(){
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(3));
+        driver.findElement(By.id("nav-hamburger-menu")).click();
 
+        wait.until(ExpectedConditions.elementToBeClickable(By.cssSelector(".hmenu-item[data-menu-id='10']")));
+        driver.findElement(By.cssSelector(".hmenu-item[data-menu-id='10']")).click();
+        
+        wait.until(ExpectedConditions.elementToBeClickable(By.cssSelector("ul.hmenu-visible > li:nth-child(3)")));
+        driver.findElement(By.cssSelector("ul.hmenu-visible > li:nth-child(3)")).click();
     }
 }
