782ef58143e613872e54873ea6eaa2455ae47379
Hassana <baazizihassana@gmail.com>
2022-01-07 12:02:03 +0100

    added amazon test file and nintendo test and test machine raclette

--- src/main/java/MainPage.java
+++ src/main/java/MainPage.java
@@ -1,9 +1,23 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.time.Duration;
 
 public class MainPage {
     WebDriver driver;
+    By searchBarSelector = By.id("twotabsearchtextbox");
+    int timeoutSearch = 0;
+    By searchResultSelector= By.cssSelector("[data-component-type='s-search-result']");
 
     public MainPage(WebDriver driver){
         this.driver =driver;
+    }
+    public void searchProduct(String productName){
+        driver.findElement(searchBarSelector).sendKeys(productName + Keys.ENTER);
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds((timeoutSearch)));
+        wait.until(ExpectedConditions.elementToBeClickable(searchResultSelector));
     }
 }
